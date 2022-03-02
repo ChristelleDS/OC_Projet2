74528fc9c7a2f37f209a09c74269724cade52a1b
ChristelleDS <christelle.dasilva75@gmail.com>
2022-03-02 12:04:55 +0100

    Update P2_script.py
    cleaning

--- P2_script.py
+++ P2_script.py
@@ -15,22 +15,6 @@ print(str("Répertoire d'export: "+ os.getcwd()) )
 #variables globales
 date = datetime.datetime.today().strftime('%Y%m%d')
 
-#FONCTIONS GLOBALES
-"""
-def extraire_donnees(elements):
-    resultat = []
-    for element in elements:
-        resultat.append(element.string)
-    return resultat
-"""
-"""
- fonction scrapper une page
-def get_soup(url):
-    reponse = requests.get(url)
-    page = reponse.content
-    soup = BeautifulSoup(page, "html.parser")
-"""
-
 ## HOMEPAGE : récupération des catégories
 url= 'http://books.toscrape.com'
 reponse = requests.get(url)
@@ -99,53 +83,4 @@ for row in categories:
         #amelioration : ajout d'une ligne nombre total de livres traités par catégories
 
 print("fin du programme")
-
-"""
-# initialisation du fichier csv exporté pour la catégorie en cours
-nom_fichier = str("f_export_"+ category['name'] +"_"+ date +".csv")
-en_tete = ["product_page_url", "universal_ product_code","title","price_including_tax","price_excluding_tax","number_available", "product_description", "category","review_rating","image_url"]
-
-def init_fichier(nom_fichier, en_tete):
-    with open(nom_fichier, 'w') as fichier_csv:
-        writer = csv.writer(fichier_csv, delimiter=',')
-        writer.writerow(en_tete)
-        print("fichier: " + nom_fichier + " initié")
-
-init_fichier(nom_fichier, en_tete)
-"""
-
-## PAGE PRODUIT
-"""
-url="http://books.toscrape.com/catalogue/the-passion-of-dolssa_351/index.html"
-reponse = requests.get(url)
-page = reponse.content
-soup = BeautifulSoup(page, "html.parser")
-url_pdt = url #page_article
-# récupération des informations sur l'article
-upc = soup.find_all('td')[0].get_text()
-product_title = soup.find("li", class_="active").get_text()
-p_ttc = soup.find_all('td')[3].get_text()
-p_ht = soup.find_all('td')[2].get_text()
-stock = str.strip(soup.find("p", class_="instock availability").get_text())
-desc =  soup.find_all('p')[3].get_text()
-description = desc.replace(';',',')
-category = str.strip(category_name) #category['name']
-rating = " a retravailler " 
-#str(soup.find("p", class_="star-rating One")).count('icon-star')
-src = soup.find("img")['src']
-img = str("http://books.toscrape.com/"+src[6:])
-# chargement des données dans le fichier d'export
-ligne = [url_pdt,upc,product_title,p_ttc,p_ht,stock,description,category,rating,img]
-print(ligne)
-
-
-nom_fichier = "test.csv"
-with open(nom_fichier, 'w',newline='', encoding="utf-8") as fichier_csv:
-    w =csv.writer(fichier_csv,delimiter=',', quotechar='"',quoting=csv.QUOTE_ALL )
-    w.writerow(ligne)
-
-"""
-
-## Récupération du tableau contenant les informations produits
-#tab_produit = soup.find('table', {'class' : 'table table-striped'})
 
